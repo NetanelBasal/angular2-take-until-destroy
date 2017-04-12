59febfe2d03ecc696622007164bda618ccea7f12
ZouAlan <alanchromecast2014@gmail.com>
2017-04-12 14:40:58 +0800

    doesn't require import all rxjs, just rxjs/Subject

--- dist/angular2-take-until-destroy.js
+++ dist/angular2-take-until-destroy.js
@@ -1,4 +1,4 @@
-import { Subject } from "rxjs";
+import { Subject } from "rxjs/Subject";
 export function TakeUntilDestroy(constructor) {
     var original = constructor.prototype.ngOnDestroy;
     var subject;
